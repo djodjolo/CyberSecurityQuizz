1b136faf24919e8e41e7313ce9700fc239e86fef
djordjemilosevic <djodjolos@gmail.com>
2019-11-17 20:48:07 +0100

    malo sredjivanje

--- app/src/main/java/com/example/kviz1/Kviz.java
+++ app/src/main/java/com/example/kviz1/Kviz.java
@@ -3,8 +3,6 @@ package com.example.kviz1;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -12,48 +10,59 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Kviz extends AppCompatActivity {
 
-    Button a1, a2, a3, a4;
+    Button a1, a2, a3;
     TextView q;
 
+    //initialize Firebase reference
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
 
+
+
+    //define JSON
+    List<JSONObject> obj=new ArrayList<JSONObject>();
+
+    List lista = new ArrayList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kviz);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference();
-
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-//                   q.append(postSnapshot.toString());
-                    q.append(postSnapshot.child("questions").toString());
-                }
-//
-            }
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                Log.e("Mapped", map.toString());
 
+
+
+
+            }
             @Override
             public void onCancelled(DatabaseError error) {
+                Log.e("cancled",error.toString());
             }
         });
-
 
         q = (TextView) findViewById(R.id.q);
         a1 = (Button) findViewById(R.id.a1);
         a2 = (Button) findViewById(R.id.a2);
         a3 = (Button) findViewById(R.id.a3);
-        a4 = (Button) findViewById(R.id.a4);
+
 
         a1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +72,12 @@ public class Kviz extends AppCompatActivity {
             }
         });
 
+        a3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Kviz.this, kraj.class));
+            }
+        });
 
     }
 
